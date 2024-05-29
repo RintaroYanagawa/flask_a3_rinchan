09f3877fda8d26763b2c6bedc673b5446e738f46
RintaroYanagawa <yanagawa.rintaro.xm@mynavi.jp>
2024-05-29 04:58:18 +0000

    feat: add database env

--- yanagawa/application/flask_blog/__init__.py
+++ yanagawa/application/flask_blog/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-import flask_blog.views  # noqa: E402
-
 app.config.from_object("flask_blog.config")
 
-__all__ = ["flask_blog"]
+db = SQLAlchemy(app)
+
+import flask_blog.views
+
