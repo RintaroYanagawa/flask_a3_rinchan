4084f886b289dd009cce418333cfce60754b3745
Saho <tamanoki.saho.py@mynavi.jp>
2024-05-31 08:30:17 +0000

    Day4

--- Tamanoki/application/flask_blog/__init__.py
+++ Tamanoki/application/flask_blog/__init__.py
@@ -1,11 +1,13 @@
 # Flask自体をインポートする
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # Flaskのアプリケーション本体を作成
 # from_object 設定を読み込むためのメゾット
 app = Flask(__name__)
 app.config.from_object('flask_blog.config')
 
+db = SQLAlchemy(app)
 
 # これから作成するviewsというファイルにインポートする
-import flask_blog.views
+from flask_blog.views import views,entries
