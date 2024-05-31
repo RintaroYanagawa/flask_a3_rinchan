5e87ff1bd376f26d15bea7aeb3e3ee22270375e6
RintaroYanagawa <yanagawa.rintaro.xm@mynavi.jp>
2024-05-31 01:05:46 +0000

    feat: add login_required

--- yanagawa/application/flask_blog/views/views.py
+++ yanagawa/application/flask_blog/views/views.py
@@ -1,5 +1,16 @@
+from functools import wraps
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
+
+
+def login_required(view):
+    @wraps(view)
+    def inner(*args, **kwargs):
+        if not session.get("logged_in"):
+            return redirect(url_for("login"))
+        return view(*args, **kwargs)
+
+    return inner
 
 
 @app.route("/login", methods=["GET", "POST"])
