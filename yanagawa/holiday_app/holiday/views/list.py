1424a93aacd7f7e07e50102dd26357a29f3b494f
RintaroYanagawa <yanagawa.rintaro.xm@mynavi.jp>
2024-05-31 08:25:02 +0000

    fix: use flash in list

--- yanagawa/holiday_app/holiday/views/list.py
+++ yanagawa/holiday_app/holiday/views/list.py
@@ -1,13 +1,18 @@
-from flask import render_template
+from flask import render_template, flash
 from holiday import app
 from holiday.models.mst_holiday import Holiday
+from holiday.utils import get_inform_message
 
 
 @app.route("/list", methods=["GET"])
 def show_list():
     holidays = Holiday.query.order_by(Holiday.holi_date.asc()).all()
-    holidays = map(
-        lambda holiday: ({"date": holiday.holi_date, "text": holiday.holi_text}),
-        holidays,
+    holidays = list(
+        map(
+            lambda holiday: ({"date": holiday.holi_date, "text": holiday.holi_text}),
+            holidays,
+        )
     )
+    if len(holidays) == 0:
+        flash(get_inform_message("I04"), "info")
     return render_template("list.html", holidays=holidays)
